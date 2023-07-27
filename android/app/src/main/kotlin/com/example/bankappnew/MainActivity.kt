9f59409c8dec96cda1a366b5d7a4a8043b024293
Yousaf <mohamedyousufdev@gmail.com>
2023-07-27 21:57:37 +0400

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/bankappnew/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bankappnew
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+}
---
+++ windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,29 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <connectivity_plus/connectivity_plus_windows_plugin.h>
+#include <local_auth_windows/local_auth_plugin.h>
+#include <modal_progress_hud_nsn/modal_progress_hud_nsn_plugin.h>
+#include <nb_utils/nb_utils_plugin.h>
+#include <smart_auth/smart_auth_plugin.h>
+#include <url_launcher_windows/url_launcher_windows.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  ConnectivityPlusWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("ConnectivityPlusWindowsPlugin"));
+  LocalAuthPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("LocalAuthPlugin"));
+  ModalProgressHudNsnPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("ModalProgressHudNsnPlugin"));
+  NbUtilsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("NbUtilsPlugin"));
+  SmartAuthPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("SmartAuthPlugin"));
+  UrlLauncherWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("UrlLauncherWindows"));
+}
